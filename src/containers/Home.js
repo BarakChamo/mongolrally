9b27897e691a4dc0a1f78199a06d4614cc190090
Barak Chamo <barakchamo@gmail.com>
2016-09-03 14:55:45 +0100

    github pages

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,11 +4,20 @@ import { inject, observer } from 'mobx-react'
 
 import { MAPS_API_KEY } from '../constants'
 
+/*
+  Map View
+*/
+
 let MapView = ({ locations }) => (
   <GoogleMap bootstrapURLKeys={{key: MAPS_API_KEY, language: 'en' }} center={locations.car} defaultZoom={15}></GoogleMap>
 )
 
 MapView = inject('locations')(observer(MapView))
+
+
+/*
+  Home View
+*/
 
 let Home = ({ locations: { reachDestination } }) => (
   <div style={{height: 500, width: 500}}>
