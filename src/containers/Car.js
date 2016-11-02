1d0b0d8012e615fcc8e23e76d751eb4f9c6ec936
Andrea Orru <andreaorru1991@gmail.com>
2016-11-02 13:32:19 -0400

    Car section.

--- src/containers/Car.js
+++ src/containers/Car.js
@@ -16,14 +16,35 @@ let Car = ({ locations: { reachDestination } }) => (
       <div className='row'>
         <div className='col-sm-12'>
           <h1 className='display-4'>Our Shit Ride</h1>
-          <h2>No AC in the gobi desert... in August.</h2>
-          <p>You all might suck balls but we're fucking awesome! LOLOL we're going to Mongolia!.</p>
-          <p>You all might suck balls but we're fucking awesome! LOLOL we're going to Mongolia!.</p>
-          <p>You all might suck balls but we're fucking awesome! LOLOL we're going to Mongolia!.</p>
-          <p>You all might suck balls but we're fucking awesome! LOLOL we're going to Mongolia!.</p>
+          <h2>No AC in the Gobi desert... in August.</h2>
         </div>
       </div>
     </section>
+
+    <section className='container-fluid content'>
+      <div className='row'>
+        <div className='col-sm-12'>
+          <p>Our holy vessel will be an old <b>Fiat Panda</b> from 2002, kindly lent (as if it's ever coming back...?) to us by Andrea's father.</p>
+        </div>
+      </div>
+    </section>
+
+    <section className='container-fluid content'>
+      <div className='row'>
+        <div className='col-sm-12'>
+          <p>The car is equipped with an all-mighty 1000cc engine in accordance with the Mongol Rally rules. It has absolutely no air conditioning whatsoever so that our appreciation of the wonderful Turkmenistan weather can be full and genuine.</p>
+        </div>
+      </div>
+    </section>
+
+    <section className='container-fluid content'>
+      <div className='row'>
+        <div className='col-sm-12'>
+          <p>Do we have any of the mechanical skills required to fix this piece of trash when it's going to break? Of course not...</p>
+        </div>
+      </div>
+    </section>
+
   </div>
 )
 
